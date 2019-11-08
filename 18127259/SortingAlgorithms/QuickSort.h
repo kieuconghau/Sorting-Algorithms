01142878b79fe521eeae041993bcbed6a80fe2f8
Kiều Công Hậu <kieuconghau.it@gmail.com>
2019-11-08 22:11:20 +0700

    Update quick sort

--- 18127259/SortingAlgorithms/QuickSort.h
+++ 18127259/SortingAlgorithms/QuickSort.h
@@ -4,8 +4,8 @@
 #include <ctime>
 using namespace std;
 
-void QuickSort(vector<int>& a, bool random_mode = 0);
+void QuickSort(vector<int>& a);
 
-void QuickSort(vector<int>& a, int left, int right, bool random_mode);
+void QuickSort(vector<int>& a, int left, int right);
 
-int Partition(vector<int>& a, int left, int right, bool random_mode);
+int Partition(vector<int>& a, int left, int right);
