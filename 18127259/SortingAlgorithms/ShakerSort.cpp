d0bcd0facadf7ec7ed4c610ea8db4eddacc7977f
Kiều Công Hậu <kieuconghau.it@gmail.com>
2019-11-12 00:01:56 +0700

    Fix Shaker sort

--- 18127259/SortingAlgorithms/ShakerSort.cpp
+++ 18127259/SortingAlgorithms/ShakerSort.cpp
@@ -1,20 +1,29 @@
 #include "ShakerSort.h"
 
 void ShakerSort(int* a, int n) {
-	for (int i = 0; i < n / 2; ++i) {
-		// Heavy
-		for (int j = i; j < n - 1 - i; ++j) {
-			if (a[j] > a[j + 1]) {
-				swap(a[j], a[j + 1]);
-			}
-		}
+	int left = 0;
+	int right = n - 1;
+	int flag;
 
-		// Light
-		for (int j = n - 2 - i; j > i; --j) {
-			if (a[j] < a[j - 1]) {
-				swap(a[j], a[j - 1]);
+	while (left < right) {
+		flag = left;
+		for (int i = left; i < right; ++i) {
+			if (a[i] > a[i + 1]) {
+				swap(a[i], a[i + 1]);
+				flag = i;
 			}
 		}
+		right = flag;
+
+
+		flag = right;
+		for (int i = right; i > left; --i) {
+			if (a[i] < a[i - 1]) {
+				swap(a[i], a[i - 1]);
+				flag = i;
+			}
+		}
+		left = flag;
 	}
 }
 
